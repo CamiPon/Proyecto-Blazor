b858b5b664e3e81cdb65223e9bcf8d374be3714e
CamiPon <camilayelenponce@gmail.com>
2021-04-30 13:08:32 -0300

    Algunos cambios en LavaderoBlazor pero luego va a ser reemplazado por LavaderoTemplateMudBlazor

---
+++ LavaderoBlazor/LavaderoBlazorBackEnd/Data/LavaderoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LavaderoBlazorBackEnd.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LavaderoBlazorBackEnd.Data
+{
+    public class LavaderoDbContext : DbContext
+    {
+        public LavaderoDbContext(DbContextOptions<LavaderoDbContext> options) : base(options)
+        { }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Lavado> Lavados { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+    }
+}
